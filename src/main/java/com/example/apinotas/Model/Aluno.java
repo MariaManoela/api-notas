e54f5a57d79a3c350d4c6d85def3d377fd9ce52a
Maria Manoela <manoelamvc@hotmail.com>
2022-02-14 17:25:17 -0300

    Create Aluno class

---
+++ src/main/java/com/example/apinotas/Model/Aluno.java
@@ -0,0 +1,30 @@
+package com.example.apinotas.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private int anoDoAluno;
+    private float notaPrimeiroTrimestre;
+    private float notaSegundoTrimestre;
+    private float notaTerceiroTrimestre;
+    private float notaFinal;
+
+}
