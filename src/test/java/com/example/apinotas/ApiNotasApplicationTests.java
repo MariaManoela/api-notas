1afad5bdf7459981070ee48463410b28ebf1e5f0
Maria Manoela <manoelamvc@hotmail.com>
2022-02-14 16:43:56 -0300

    :tada: First commit

---
+++ src/test/java/com/example/apinotas/ApiNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apinotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
