fd07b1837e681f2d89006a986f830884281dc5e4
Maria Manoela <manoelamvc@hotmail.com>
2022-02-14 17:36:33 -0300

    Create controller and service files

---
+++ src/main/java/com/example/apinotas/Controller/AlunoController.java
@@ -0,0 +1,9 @@
+package com.example.apinotas.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlunoController {
+
+
+}
---
+++ src/main/java/com/example/apinotas/Service/AlunoService.java
@@ -0,0 +1,8 @@
+package com.example.apinotas.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlunoService {
+
+}
