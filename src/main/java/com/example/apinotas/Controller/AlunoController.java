1c961536ef41bcfaf6595545f55aa01d1fb24f57
Maria Manoela <manoelamvc@hotmail.com>
2022-02-15 09:24:22 -0300

    Add annotations and @GetMapping

--- src/main/java/com/example/apinotas/Controller/AlunoController.java
+++ src/main/java/com/example/apinotas/Controller/AlunoController.java
@@ -1,9 +1,22 @@
 package com.example.apinotas.Controller;
 
+import com.example.apinotas.Service.AlunoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/alunos")
 public class AlunoController {
 
+    private final AlunoService alunoService;
 
+    @GetMapping
+    public List findAll() {
+        return alunoService.findAll();
+    }
 }
