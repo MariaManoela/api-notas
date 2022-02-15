2aa313f0e5d062c65176d1d605ee4c9d1871a5cd
Maria Manoela <manoelamvc@hotmail.com>
2022-02-15 16:14:42 -0300

    Initialize test development

--- src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
+++ src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
@@ -1,5 +1,21 @@
 package com.example.apinotas.Controller;
 
+import com.example.apinotas.Model.Aluno;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = AlunoController.class)
 public class AlunoControllerTest {
 
+    private AlunoController alunoController;
+
+    @Mock
+    private Aluno aluno;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.openMocks(this);
+    }
 }
