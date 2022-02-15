0ea6bfe22bc01a8122b04dd33ff877ac21226b16
Maria Manoela <manoelamvc@hotmail.com>
2022-02-15 09:23:45 -0300

    Create Repository interface

---
+++ src/main/java/com/example/apinotas/Repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.example.apinotas.Repository;
+
+import com.example.apinotas.Model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
