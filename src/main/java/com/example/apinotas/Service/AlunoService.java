173f042e24815301e61215fc9848c5bb54747056
Maria Manoela <manoelamvc@hotmail.com>
2022-02-15 09:25:07 -0300

    Create findAll method and add annotations

--- src/main/java/com/example/apinotas/Service/AlunoService.java
+++ src/main/java/com/example/apinotas/Service/AlunoService.java
@@ -1,8 +1,18 @@
 package com.example.apinotas.Service;
 
+import com.example.apinotas.Repository.AlunoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class AlunoService {
 
+    private final AlunoRepository alunoRepository;
+
+    public static List findAll() {
+        return alunoRepository.findAll();
+    }
 }
