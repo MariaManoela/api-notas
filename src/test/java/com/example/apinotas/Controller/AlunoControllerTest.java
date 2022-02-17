e16ec3a3dfa27c05859f06791bbd278268828136
Maria Manoela <manoelamvc@hotmail.com>
2022-02-17 13:52:24 -0300

    Correct annotations and create list

--- src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
+++ src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
@@ -1,21 +1,48 @@
 package com.example.apinotas.Controller;
 
 import com.example.apinotas.Model.Aluno;
+import com.example.apinotas.Service.AlunoService;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest(controllers = AlunoController.class)
 public class AlunoControllerTest {
 
-    private AlunoController alunoController;
+    List<Aluno> lista = new ArrayList<>();
 
-    @Mock
-    private Aluno aluno;
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    AlunoService alunoService;
+
 
     @BeforeEach
     public void beforeEach() {
         MockitoAnnotations.openMocks(this);
+        Aluno aluno1 = new Aluno(1L, "Manoela", 9, 8.4f, 9.2f, 8.7f, 8.76f,true );
+        Aluno aluno2 = new Aluno(2L, "Pedro", 7, 6.4f, 7.1f, 7.3f, 6.93f, false);
+        Aluno aluno3 = new Aluno(3L, "Camila", 5, 8.8f, 8.2f, 8.5f, 8.5f, true);
+
+        lista.add(aluno1);
+        lista.add(aluno2);
+        lista.add(aluno3);
+    }
+
+    @Test
+    public void deveriaMostrarTodosAlunosComSucesso() {
+        Mockito.when(alunoService.findAll()).thenReturn(lista);
+        Assert.assertEquals(lista, alunoService.findAll());
+        System.out.println(alunoService.findAll());
     }
 }
