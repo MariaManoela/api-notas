9247fbdc6fe80a4eba19ff7ff34536438af8e779
Maria Manoela <manoelamvc@hotmail.com>
2022-02-17 15:51:21 -0300

    Create service test class

---
+++ src/test/java/com/example/apinotas/Service/AlunoServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.apinotas.Service;
+
+import com.example.apinotas.Repository.AlunoRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AlunoServiceTest {
+
+    @Mock
+    private AlunoRepository alunoRepository;
+
+    @InjectMocks
+    private AlunoService alunoService;
+}
