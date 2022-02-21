23bfec8878da40140cf122ef38c56109ca8c3b0e
Maria Manoela <manoelamvc@hotmail.com>
2022-02-21 14:05:12 -0300

    Correct controller test

--- src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
+++ src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.apinotas.Controller;
 
+import com.example.apinotas.Model.Aluno;
 import com.example.apinotas.Service.AlunoService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -9,6 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest(controllers = AlunoController.class)
 public class AlunoControllerTest {
@@ -21,18 +26,28 @@ public class AlunoControllerTest {
 
     @Test
     public void retornarListaDeAlunos() throws Exception {
+        Aluno aluno1 = new Aluno(1L, "Maria", 7, 6.4f, 7.2f, 6.7f, 6.76f,false);
+        Aluno aluno2 = new Aluno(2L, "Pedro", 9, 8.4f, 9.2f, 8.7f, 8.76f,true);
+
+        List<Aluno> listaAlunos = new ArrayList<Aluno>();
+        listaAlunos.add(aluno1);
+        listaAlunos.add(aluno2);
+
+        Mockito.when(alunoService.findAll()).thenReturn(listaAlunos);
         this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+
+
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
+//                .andDo(MockMvcResultHandlers.print())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
 
         //        ---------------se encaixa em teste de controller?---------------
 //        List<Aluno> listaAlunos = new ArrayList<>();
 //        listaAlunos.add(new Aluno(1L, "Manoela", 9, 8.4f, 9.2f, 8.7f, 8.76f,true));
 //
-//        Mockito.when(alunoService.findAll()).thenReturn(listaAlunos);
-//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
-//                .andExpect(MockMvcResultMatchers.status().isOk());
 //
-
     }
 }
