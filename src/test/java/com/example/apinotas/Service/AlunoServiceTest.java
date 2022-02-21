4822ac0e4fe263e342da306068950ff1c8c1dcc2
Maria Manoela <manoelamvc@hotmail.com>
2022-02-21 11:04:50 -0300

    Add retornarAlunoBuscandoPeloId test

--- src/test/java/com/example/apinotas/Service/AlunoServiceTest.java
+++ src/test/java/com/example/apinotas/Service/AlunoServiceTest.java
@@ -35,4 +35,17 @@ public class AlunoServiceTest {
         assertEquals(alunoService.findAll(), lista);
         verify(alunoRepository).findAll();
     }
+
+    @Test
+    void retornarAlunoBuscandoPeloId() {
+        List<Aluno> lista = new ArrayList<>();
+        Aluno aluno1 = new Aluno(1L, "Manoela", 9, 8.4f, 9.2f, 8.7f, 8.76f,true);
+
+        lista.add(aluno1);
+        lista.add(new Aluno(2L, "Camila", 8, 6.8f, 7.5f, 6.9f, 7.06f,true));
+
+        when(alunoRepository.findById(2L)).thenReturn(java.util.Optional.of(aluno1));
+        assertEquals(alunoService.findById(2L), aluno1);
+        verify(alunoRepository).findById(2L);
+    }
 }
