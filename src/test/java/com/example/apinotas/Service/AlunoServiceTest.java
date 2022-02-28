c3db0407e9c560bc6251391f8f488c9a07e095b5
Maria Manoela <manoelamvc@hotmail.com>
2022-02-28 12:11:59 -0300

    Remove public from method

--- src/test/java/com/example/apinotas/Service/AlunoServiceTest.java
+++ src/test/java/com/example/apinotas/Service/AlunoServiceTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class AlunoServiceTest {
+class AlunoServiceTest {
 
     @Mock
     private AlunoRepository alunoRepository;
