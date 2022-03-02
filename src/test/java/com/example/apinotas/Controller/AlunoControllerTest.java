c27260b355d8747ab18828cb615902ac0769518d
Maria Manoela <manoelamvc@hotmail.com>
2022-03-02 15:22:49 -0300

    Correct tests

--- src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
+++ src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
@@ -14,13 +14,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest(controllers = AlunoController.class)
 class AlunoControllerTest {
@@ -31,44 +27,44 @@ class AlunoControllerTest {
     @MockBean
     AlunoService alunoService;
 
-//    @Test
-//    void retornarListaDeAlunos() throws Exception {
-//
-//       // Aluno aluno2 = new Aluno(2L, "Pedro", 9, 8.4f, 9.2f, 8.7f, 8.76f,true);
-//        mockMvc = Mockito.when(Collections.singletonList(buildAluno()));
-////        List<Aluno> listaAlunos = new ArrayList<Aluno>();
-////        listaAlunos.add(aluno1);
-////        listaAlunos.add(aluno2);
-//
-//        Mockito.when(alunoService.findAll()).thenReturn(listaAlunos);
-//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(content().string(listaAlunos.toString()));
-
-//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
-//
-//    }
-
     public Aluno buildAluno(){
         return new Aluno(1L, "Maria", 7, 6.4f, 7.2f, 6.7f, 6.76f,false);
     }
 
     @Test
+    void retornarListaDeAlunos() throws Exception {
+        List<Aluno> esperado = Collections.singletonList(buildAluno());
+
+        Mockito.when(alunoService.findAll()).thenReturn(esperado);
+
+        List<Aluno> resultado = alunoService.findAll();
+        Assertions.assertEquals(esperado,resultado);
+
+//        Mockito.when(alunoService.findAll()).thenReturn(listaAlunos);
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
+//                .andDo(MockMvcResultHandlers.print())
+//                .andExpect(content().string(listaAlunos.toString()));
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
+//                .andDo(MockMvcResultHandlers.print())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
+
+    }
+
+    @Test
     void buscarAlunoPeloId() throws Exception {
-        Aluno aluno1 = new Aluno(1L, "Maria", 7, 6.4f, 7.2f, 6.7f, 6.76f,false);
-        Aluno aluno2 = new Aluno(2L, "Pedro", 9, 8.4f, 9.2f, 8.7f, 8.76f,true);
+        Optional<Aluno> esperado = Optional.of(buildAluno());
 
-        List<Aluno> listaAlunos = new ArrayList<Aluno>();
-        listaAlunos.add(aluno1);
-        listaAlunos.add(aluno2);
+        Mockito.when(alunoService.findById(1L)).thenReturn(esperado);
 
-//        Mockito.when(alunoService.findById(2L)).thenReturn(Optional.of(aluno1));
-        Assertions.assertEquals(Optional.of(aluno1), alunoService.findById(1L));
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+        Optional<Aluno> resultado = alunoService.findById(1L);
+        Assertions.assertEquals(esperado, resultado);
+//
+////        Mockito.when(alunoService.findById(2L)).thenReturn(Optional.of(aluno1));
+//        Assertions.assertEquals(Optional.of(aluno1), alunoService.findById(1L));
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos/1"))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
