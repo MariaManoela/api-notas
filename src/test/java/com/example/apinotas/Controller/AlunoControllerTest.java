ec1c1087ba91617b18fb84ce36525205da835612
Maria Manoela <manoelamvc@hotmail.com>
2022-03-03 14:56:25 -0300

    Adjust tests

--- src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
+++ src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
@@ -9,7 +9,9 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -17,6 +19,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(controllers = AlunoController.class)
 class AlunoControllerTest {
@@ -68,29 +74,49 @@ class AlunoControllerTest {
     }
 
     @Test
-    void cadastrarAluno() throws Exception{
+    void cadastrarAluno() throws Exception {
         Aluno aluno1 = new Aluno(1L, "Maria", 7, 6.4f, 7.2f, 6.7f, 6.76f,false);
 
         String paraJson = new Gson().toJson(aluno1);
 
         Mockito.when(alunoService.save(aluno1)).thenReturn(aluno1);
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos"))
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/alunos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(paraJson))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect((ResultMatcher) jsonPath("$.id", is(1L)))
+                .andExpect((ResultMatcher) jsonPath("$.nome", is("Maria")))
+                .andExpect((ResultMatcher) jsonPath("$.anoDoAluno", is(7)))
+                .andExpect((ResultMatcher) jsonPath("$.notaPrimeiroTrimestre", is(6.4f)))
+                .andExpect((ResultMatcher) jsonPath("$.notaSegundoTrimestre", is(7.2f)))
+                .andExpect((ResultMatcher) jsonPath("$.notaTerceiroTrimestre", is(6.7f)))
+                .andExpect((ResultMatcher) jsonPath("$.notaFinal", is(6.76f)))
+                .andExpect((ResultMatcher) jsonPath("$.aprovado", is(false)))
                 .andDo(MockMvcResultHandlers.print());
+
+        verify(alunoService).save(aluno1);
+
+//        List<Aluno> esperado = Collections.singletonList(buildAluno());
+//        Aluno esperado = new Aluno(1L, "Maria", 7, 6.4f, 7.2f, 6.7f, 6.76f,false);
+//
+//        Mockito.when(alunoService.save(esperado)).thenReturn(esperado);
+//
+//        Aluno resultado = alunoService.save(esperado);
+//        Assertions.assertEquals(esperado, resultado);
     }
 
-//    @Test
-//    public void deletarAluno() throws Exception {
-//        Aluno aluno1 = new Aluno(1L, "Maria", 7, 6.4f, 7.2f, 6.7f, 6.76f,false);
-//        Aluno aluno2 = new Aluno(2L, "Pedro", 9, 8.4f, 9.2f, 8.7f, 8.76f,true);
+    @Test
+    public void deletarAluno() throws Exception {
+        Aluno aluno = new Aluno(1L, "Maria", 7, 6.4f, 7.2f, 6.7f, 6.76f,false);
+
+        Mockito.when(alunoService.findById(aluno.getId())).thenReturn(Optional.of(aluno));
+
+        alunoService.deleteById(aluno.getId());
+        verify(alunoService).deleteById(aluno.getId());
 //
-//        List<Aluno> listaAlunos = new ArrayList<Aluno>();
-//        listaAlunos.add(aluno1);
-//        listaAlunos.add(aluno2);
-//
-//        Mockito.when(alunoService.deleteById(1L)).thenReturn(null);
-//        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos/{id}"))
-//                .andExpect(MockMvcResultMatchers.status().isCreated())
-//                .andDo(MockMvcResultHandlers.print());
-//    }
+////        Mockito.when(alunoService.deleteById(1L)).thenReturn(null);
+////        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos/{id}"))
+////                .andExpect(MockMvcResultMatchers.status().isCreated())
+////                .andDo(MockMvcResultHandlers.print());
+    }
 }
