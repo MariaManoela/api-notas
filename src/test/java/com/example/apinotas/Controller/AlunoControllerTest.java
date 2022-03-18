eb76c9c1b2cbca5f35a82523d004e9b30b517fa8
Maria Manoela <manoelamvc@hotmail.com>
2022-03-18 13:42:12 -0300

    Adjust buildAluno method

--- src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
+++ src/test/java/com/example/apinotas/Controller/AlunoControllerTest.java
@@ -3,7 +3,9 @@ package com.example.apinotas.Controller;
 import com.example.apinotas.Model.Aluno;
 import com.example.apinotas.Service.AlunoService;
 import com.google.gson.Gson;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,9 @@ import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = AlunoController.class)
 class AlunoControllerTest {
@@ -36,14 +38,19 @@ class AlunoControllerTest {
     @MockBean
     AlunoService alunoService;
 
+    public static Aluno buidAluno() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Maria");
+        aluno.setAnoDoAluno(9);
+        aluno.setNotaPrimeiroTrimestre(9f);
+        aluno.setNotaSegundoTrimestre(8.7f);
+        aluno.setNotaTerceiroTrimestre(8.9f);
+        return aluno;
+    }
+
    @Test
     void retornarSucessoNoCadastroDeAluno() {
-       Aluno aluno = new Aluno();
-       aluno.setNome("Maria");
-       aluno.setAnoDoAluno(9);
-       aluno.setNotaPrimeiroTrimestre(9f);
-       aluno.setNotaSegundoTrimestre(8.7f);
-       aluno.setNotaTerceiroTrimestre(8.9f);
+       Aluno aluno = buidAluno();
        Aluno alunoSalvo = new Aluno(1L, "Maria", 9, 9f, 8.7f, 8.9f, 8.86f, true);
        when(alunoService.cadastraAluno(aluno)).thenReturn(alunoSalvo);
 
