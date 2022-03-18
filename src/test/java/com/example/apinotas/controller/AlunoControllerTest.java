59ab436c7f6a3d903afce02c212046541113f625
Maria Manoela <manoelamvc@hotmail.com>
2022-03-18 13:46:45 -0300

    Remove unused imports

--- src/test/java/com/example/apinotas/controller/AlunoControllerTest.java
+++ src/test/java/com/example/apinotas/controller/AlunoControllerTest.java
@@ -8,10 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(controllers = AlunoController.class)
 class AlunoControllerTest {
--- src/test/java/com/example/apinotas/service/AlunoServiceTest.java
+++ src/test/java/com/example/apinotas/service/AlunoServiceTest.java
@@ -10,11 +10,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(MockitoExtension.class)
 class AlunoServiceTest {
